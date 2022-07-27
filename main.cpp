fe5314a1e7cc76635d7c08dd41888c92d3434b84
Jasper <mr.jianping.dong@Gmail.com>
2022-07-27 15:34:13 +0200

    Feature - intent word

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+#include "src/intent.h"
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    // get input sentence
+    //string input = "What is the weather like today?";
+    //string input = "What is the weather like in Paris today?";
+    string input = "Tell me an interesting fact.";
+    //string input = "Tell me a good example.";
+    // getline (cin, input);
+    cout << input << endl;
+
+    // extract intent word
+    string word = extract_intent_word(input);
+    cout << word;
+
     return 0;
 }
